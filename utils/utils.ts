86137856fb802f1938536235c21508bd8f71bd1b
Johnny Jiang <johnny.nan.jiang@gmail.com>
2023-06-13 19:17:31 +0800

    - add missing utils/utils.ts

---
+++ utils/utils.ts
@@ -0,0 +1,51 @@
+const bs58 = require("bs58");
+const ethers = require("ethers");
+
+function int2ip(ipInt: number) {
+    return (
+        (ipInt >>> 24) +
+        "." +
+        ((ipInt >> 16) & 255) +
+        "." +
+        ((ipInt >> 8) & 255) +
+        "." +
+        (ipInt & 255)
+    );
+}
+
+function ip2int(ip: string) {
+    return (
+        ip.split(".").reduce(function (ipInt, octet) {
+            return (ipInt << 8) + parseInt(octet, 10);
+        }, 0) >>> 0
+    );
+}
+
+/**
+ * Partition multihash string into object representing multihash
+ *
+ * @param {string} multihash A base58 encoded multihash string
+ * @returns {Multihash}
+ */
+function getBytesFromMultihash(multihash: any) {
+    const decoded = bs58.decode(multihash);
+
+    return `0x${Buffer.from(decoded).toString("hex")}`;
+}
+
+/**
+ * Partition multihash string into object representing multihash
+ *
+ * @param {string} multihash A base58 encoded multihash string
+ * @returns {Multihash}
+ */
+function getMultihashFromBytes(bytes: any) {
+    return bs58.encode(bytes);
+}
+
+module.exports = {
+    int2ip,
+    ip2int,
+    getMultihashFromBytes,
+    getBytesFromMultihash,
+};
